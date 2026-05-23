d2a4645b77d143772f29ec1aac21e9c384871ee8
andyxsly <andyxsly@bk.ru>
2026-05-23 05:10:09 +0300

    J

---
+++ scripts421.sql
@@ -0,0 +1,14 @@
+ALTER TABLE student
+ADD CONSTRAINT age_constraint CHECK (age >= 16);
+
+ALTER TABLE student
+ADD CONSTRAINT name_unique UNIQUE (name);
+
+ALTER  TABLE student
+ALTER COLUMN name SET NOT NULL;
+
+ALTER TABLE student
+ADD CONSTRAINT id_color_unique UNIQUE (id, color);
+
+ALTER TABLE student
+ALTER COLUMN age SET DEFAULT 20;
